d870f43dcd8547c44c30f7603a152526f1dab0e8
cy <cydial89@gmail.com>
2024-10-03 21:27:12 +0900

    feat : 레진 과제 프로젝트 디폴트 설정

---
+++ src/main/java/com/lezhintask/LezhinTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lezhintask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezhinTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LezhinTaskApplication.class, args);
+	}
+
+}
